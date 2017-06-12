c9133df449c201b13c2bca5a84b102eb4bfa3640
aoki-hid <aoki-hid@spp.co.jp>
2017-06-12 13:48:55 +0900

    Modify 三角形コードを表示しやすいように修正

--- RightTriangle_for_Linq.cs
+++ RightTriangle_for_Linq.cs
@@ -5,7 +5,15 @@ using System;
 public class Hello{
     public static void Main(){
 		//
-        var ans = Enumerable.Range(1, 10).SelectMany(x => Enumerable.Range(1, x).SelectMany(y => Enumerable.Range(1, y).Where(z => x + y + z == 24).Where(z => x * x == y * y + z * z).Select(z=>new List<int> { x, y, z }))).ToList();
-        ans.ForEach(_=>_.ForEach(x=>Console.Write(x+" ")));
+		var ans = 
+			Enumerable.Range(1, 10)
+            .SelectMany(_ => Enumerable.Range(1, _), (a, b) => new { a, b })
+            .SelectMany(_ => Enumerable.Range(1, _.b), (_, c) => new { _.a, _.b, c })
+            .Where(_ => _.a + _.b + _.c == 24)
+            .Where(_ => _.a * _.a == _.b * _.b + _.c * _.c)
+            .Select(_ => new List<string> { _.a.ToString(), _.b.ToString(), _.c.ToString() });
+
+		var ansString = ans.Select(_ => _.Aggregate((a, b) => a + ",  + b));
+        foreach (var i in ansString) { Console.WriteLine(i); }
     }
 }
--- RightTriangle_for_query.cs
+++ RightTriangle_for_query.cs
@@ -5,14 +5,15 @@ using System.Linq;
 public class Hello{
     public static void Main(){
         // クエリバージョン
-        var ans =
-        from x in Enumerable.Range(1, 10)
-        from y in Enumerable.Range(1, x)
-        from z in Enumerable.Range(1, y)
-        where (x + y + z == 24)
-        where (x * x == y * y + z * z)
-        select new List<int>() { x, y, z };
-        
-        ans.ToList().ForEach(x => x.ForEach(y => Console.Write(y+" ")));
+		var ans =
+			from x in Enumerable.Range(1, 10)
+            from y in Enumerable.Range(1, x)
+            from z in Enumerable.Range(1, y)
+            where (x + y + z == 24)
+            where (x * x == y * y + z * z)
+            select new List<string>() { x.ToString(), y.ToString(), z.ToString() };
+
+            var ansString = ans.Select(_ => _.Aggregate((a, b) => a + ", " + b));
+            foreach (var i in ansString) { Console.WriteLine(i); }
     }
 }
