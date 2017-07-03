64fb4653646b3b96cc1d2ff186acd520c2acc636
aoki-hid <aoki-hid@spp.co.jp>
2017-07-03 15:18:38 +0900

    Modify アルファベットカウント

--- AlphabetCount/AlphabetCount/Program.cs
+++ AlphabetCount/AlphabetCount/Program.cs
@@ -7,38 +7,41 @@ namespace AlphabetCount
 {
     class Program
     {
-        static string Pangram = "the quick brown fox jumps over the lazy dog";
-
         static void Main(string[] args)
         {
-            CountLinq();
+            string Pangram = "the quick brown fox jumps over the lazy dog";
 
-            CountNormal();
+            // Linq
+            CountLinq(Pangram);
+
+            // Normal
+            CountNormal(Pangram);
 
             while (true) { }
         }
 
         // LINQ
-        static void CountLinq()
+        static void CountLinq(string str)
         {
             // 文章から空白を削除
-            IEnumerable<char> s1 = Regex.Replace(Pangram, @"\s", "");
+            IEnumerable<char> s1 = Regex.Replace(str, @"\s", "");
             Console.WriteLine("LINQ");
             Console.WriteLine(s1.OrderBy(x => x).Distinct().Select(x => x.ToString() + ":" + s1.Count(y => y == x)).Aggregate((a, b) => a + ", " + b));
         }
 
         // Normal
-        static void CountNormal()
+        static void CountNormal(string str)
         {
             var dic = new Dictionary<char, int>();
 
-            foreach(var i in Regex.Replace(Pangram, @"\s", ""))
+            foreach (var i in Regex.Replace(str, @"\s", ""))
             {
                 if (dic.ContainsKey(i)) { dic[i]++; }
                 else { dic.Add(i, 1); }
             }
+
             Console.WriteLine("Normal");
-            Console.WriteLine(dic.OrderBy(x=>x.Key).Select(x => x.ToString()).Aggregate((a, b) => a + ", " + b));
+            Console.WriteLine(dic.OrderBy(x => x.Key).Select(x => x.ToString()).Aggregate((a, b) => a + ", " + b));
         }
     }
 }
