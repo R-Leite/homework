3e95596ab47ae7255d021ce61aaf219125bec2ea
Leite <chemistry_nothing@hotmail.co.jp>
2017-07-10 00:37:18 +0900

    Modify EmployeeDictionary

--- EmployeeDictionary/EmployeeDictionary/Program.cs
+++ EmployeeDictionary/EmployeeDictionary/Program.cs
@@ -12,32 +12,29 @@ namespace EmployeeDictionary
         {
             var employeeData = new List<string>() { "19336", "青木秀樹", "19337", "岩本康宏", "69210", "メーカインウー", "69281", "宇賀勇太" };
 
-            var ans = employeeData.Select((x, i) => new { x, i }).GroupBy(x => (int)(x.i / 2), x => x.x).ToDictionary(x=>x.First(), x=>x.Skip(1).First());
+            PrintAnswer(CreateEmployeeDictionary(employeeData));
 
-            foreach(var i in ans)
-            {
-                Console.WriteLine(i.Key);
-                Console.WriteLine(i.Value);
-            }
-
-            var tab = new List<Dictionary<string, string>>();
-            var dic = new Dictionary<string, string>();
-            var dic2 = new Dictionary<string, string>();
-            dic.Add("a", "hoge");
-            dic.Add("b", "fuga");
-            dic2.Add("a", "h");
-            dic2.Add("b", "f");
-            tab.Add(dic);
-            tab.Add(dic2);
-
-            var foo = tab.Select(x => x.Select(y => y.Key + ", " + y.Value).Aggregate((a, b) => a + ", " + b) + ", " + "a" + "b" + ", "+ x["a"] + x["b"]);
-
-            foreach(var i in foo)
-            {
-                Console.WriteLine(i);
-            }
+            PrintAnswer(Linq(employeeData));
 
             while (true) { }
+        }
+
+        static void PrintAnswer(Dictionary<string, string> dictionary)
+        {
+            Console.WriteLine(dictionary.Select(x => x.ToString()).Aggregate((a, b) => a + ", " + b));
+        }
+
+        static Dictionary<string, string> CreateEmployeeDictionary(List<string> employeeData)
+        {
+            return employeeData.Select((val, idx) => new { val, idx }).GroupBy(x => (int)(x.idx / 2), x => x.val).ToDictionary(x => x.FirstOrDefault(), x => x.Skip(1).FirstOrDefault());
+        }
+
+        static Dictionary<string, string> Linq(List<string> employeeData)
+        {
+            var k = employeeData.Where((val, idx) => (idx % 2 == 0));
+            var v = employeeData.Where((val, idx) => (idx % 2 == 1));
+
+            return k.Zip(v, (key, val) => new { key, val }).ToDictionary(x => x.key, x => x.val);
         }
     }
 }
---
+++ TableInsert/TableInsert/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableInsert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tab = new List<Dictionary<string, string>>();
+            var dic = new Dictionary<string, string>();
+            var dic2 = new Dictionary<string, string>();
+            dic.Add("a", "hoge");
+            dic.Add("b", "fuga");
+            dic2.Add("a", "h");
+            dic2.Add("b", "f");
+            tab.Add(dic);
+            tab.Add(dic2);
+
+            var foo = tab.Select(x => x.Select(y => y.Key + ", " + y.Value).Aggregate((a, b) => a + ", " + b) + ", " + "a" + "b" + ", " + x["a"] + x["b"]);
+
+            foreach (var i in foo)
+            {
+                Console.WriteLine(i);
+            }
+
+            while (true) { }
+        }
+    }
+}
