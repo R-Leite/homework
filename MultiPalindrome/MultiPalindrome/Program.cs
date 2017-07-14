9f7080b4c40f30150ef108812475d347682eff56
aoki-hid <aoki-hid@spp.co.jp>
2017-07-14 13:29:12 +0900

    Add n桁同士の積が左右反転しても同じになる最大値を求める

---
+++ MultiPalindrome/MultiPalindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetMax(2);
+
+            while (true) { }
+        }
+
+        static int GetMax(int digit)
+        {
+            var maxnum = (digit == 2) ? 99 : 999;
+            if(digit==2)
+            {
+
+            }
+            var aa = "abcde";
+            Console.WriteLine(aa);
+            string bb = aa.Reverse().ToString();
+            Console.WriteLine(bb);
+            var list = Enumerable.Range(10, 90).SelectMany(x => Enumerable.Range(10, 90).Select(y => x * y)).Where(x=>x.ToString()==x.ToString().Reverse().ToString());
+
+            foreach(var i in list)
+            {
+                Console.WriteLine(i);
+            }
+            return 1;
+        }
+    }
+}
---
+++ MultiPalindrome/MultiPalindrome/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("MultiPalindrome")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("MultiPalindrome")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("bd658f4b-71bf-4632-bcf3-48fe62aea632")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
