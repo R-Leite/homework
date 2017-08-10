d516628ee0ddd8bea2a034560283ec3708a2107a
aoki-hid <aoki-hid@spp.co.jp>
2017-08-10 18:46:09 +0900

    Add AokiQuest

---
+++ AokiQuest/AokiQuest/Map.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AokiQuest
+{
+    class Map
+    {
+        private int max_x;
+        private int max_y;
+
+        public Map()
+        {
+            max_x = 10;
+            max_y = 10;
+        }
+
+        public void Render()
+        {
+            while(true)
+            {
+                var map = new List<string>();
+                for (var y = 0; y < max_y; y++)
+                {
+                    for (var x = 0; x < max_x; x++)
+                    {
+                        if (y == 0 || y == max_y - 1)
+                        {
+                            map.Add("-");
+                        }
+                        else if (x == 0 || x == max_x - 1)
+                        {
+                            map.Add("|");
+                        }
+                        else
+                        {
+                            map.Add(" ");
+                        }
+                    }
+                    map.Add("\n");
+                }
+
+                var m = map.Aggregate((a, b) => a + b);
+
+                Console.WriteLine(m);
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ AokiQuest/AokiQuest/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AokiQuest
+{
+    public class Player
+    {
+        private int _x;
+        private int _y;
+        private string _icon = "^";
+
+        public Player(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public void Walk(int direction)
+        {
+            switch(direction)
+            {
+                case 2:
+                        _y++;
+                        break;
+                case 4:
+                    _x--;
+                    break;
+                case 6:
+                    _x++;
+                    break;
+                case 8:
+                    _y--;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
