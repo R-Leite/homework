d39259abbc7963b9f044a0972f64187beb85ba57
aoki-hid <aoki-hid@spp.co.jp>
2017-08-10 11:51:52 +0900

    Modify Compatibility

--- Compatibility/Compatibility/Program.cs
+++ Compatibility/Compatibility/Program.cs
@@ -8,25 +8,24 @@ namespace Compatibility
 {
     class Program
     {
+        static string _space = "";
+
         static void Main(string[] args)
         {
             var first = new List<int>() { 6, 8, 7, 7 };
             var second = new List<int>() { 5, 7, 9, 4, 5 };
 
-            var sa = FortuneTelling(first.Concat(second));
-
-            foreach(var i in sa)
-            {
-                Console.Write(i);
-            }
+            Console.WriteLine("相性:" + int.Parse(FortuneTelling(first.Concat(second)).Select(x => x.ToString()).Aggregate((a, b) => a + b)) + "%");
 
             while (true) { }
         }
 
+        // 相性を計算する関数
         static IEnumerable<int> FortuneTelling(IEnumerable<int> StrokeCount)
         {
-            foreach(var i in StrokeCount) { Console.Write(i + " "); }
-            Console.WriteLine();
+            Console.Write(_space += " ");
+            Console.WriteLine(StrokeCount.Select(x => x.ToString()).Aggregate((a, b) => a + " " + b));
+
             if (int.Parse(StrokeCount.Select(x => x.ToString()).Aggregate((a, b) => a + b)) <= 100) { return StrokeCount; }
             return FortuneTelling(StrokeCount.Zip(StrokeCount.Skip(1), Tuple.Create).Select(x => (x.Item1 + x.Item2) % 10));
         }
