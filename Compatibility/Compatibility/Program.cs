7ab8eff1b28b56ce8ca42df3f089878730b8a3a6
aoki-hid <aoki-hid@spp.co.jp>
2017-08-22 18:31:12 +0900

    Modify Compatibility

--- Compatibility/Compatibility/Program.cs
+++ Compatibility/Compatibility/Program.cs
@@ -10,10 +10,12 @@ namespace Compatibility
 
         static void Main(string[] args)
         {
-            var first = new List<int>() { 5, 1, 4, 5 };
-            var second = new List<int>() { 1, 6, 9, 6 };
+            var first = "5,1,4,5";
+            var second = "1,6,9,6";
 
-            Console.WriteLine(Environment.NewLine + "相性:" + int.Parse(FortuneTelling(first.Concat(second)).Select(x => x.ToString()).Aggregate((a, b) => a + b)) + "%");
+            var strokeConcat = first.Split(',').Concat(second.Split(',')).Select(x => int.Parse(x));
+
+            Console.WriteLine(Environment.NewLine + "相性:" + int.Parse(FortuneTelling(strokeConcat).Select(x => x.ToString()).Aggregate((a, b) => a + b)) + "%");
 
             while (true) { }
         }
@@ -21,9 +23,8 @@ namespace Compatibility
         // 相性を計算する関数
         static IEnumerable<int> FortuneTelling(IEnumerable<int> StrokeCount)
         {
-            Console.Write(_space += " ");
-            Console.WriteLine(StrokeCount.Select(x => x.ToString()).Aggregate((a, b) => a + " " + b));
-
+            //Console.Write(_space += " ");
+            //Console.WriteLine(StrokeCount.Select(x => x.ToString()).Aggregate((a, b) => a + " " + b));
             if (long.Parse(StrokeCount.Select(x => x.ToString()).Aggregate((a, b) => a + b)) <= 100) { return StrokeCount; }
             return FortuneTelling(StrokeCount.Zip(StrokeCount.Skip(1), Tuple.Create).Select(x => (x.Item1 + x.Item2) % 10));
         }
