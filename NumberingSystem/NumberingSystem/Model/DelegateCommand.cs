e08820326ffb4367ae4b854d5b941312fca62b7a
aoki-hid <aoki-hid@spp.co.jp>
2017-09-07 18:49:02 +0900

    Modify NumberingSystem

---
+++ NumberingSystem/NumberingSystem/Model/DelegateCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace NumberingSystem.Model
+{
+    class DelegateCommand : ICommand
+    {
+        Action execute;
+        Func<bool> canExecute;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+    }
+}
--- NumberingSystem/NumberingSystem/ViewModel/MainViewModel.cs
+++ NumberingSystem/NumberingSystem/ViewModel/MainViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NumberingSystem.Model;
 
 namespace NumberingSystem.ViewModel
 {
@@ -17,9 +19,31 @@ namespace NumberingSystem.ViewModel
         }
         #endregion
 
+        #region コマンドプロパティ
+        // 番号取得
+        public DelegateCommand GetNumberCommand { get; set; }
+        #endregion
+
+        #region コンストラクタ
         public MainViewModel()
         {
-            this.Label = "hogehoge";
+            this.Label = "0";
+
+            Action actionGetNumber = this.GetNumber;
+            this.GetNumberCommand = new DelegateCommand(actionGetNumber, () => true);
+
+            var hoge = Properties.Resources.Number;
+
+            MessageBox.Show(hoge);
         }
+        #endregion
+
+        #region 番号取得
+        private void GetNumber()
+        {
+            this.Label = (int.Parse(this.Label) + 1).ToString();
+        }
+        #endregion
+
     }
 }
