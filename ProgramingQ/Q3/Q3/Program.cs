03e7d51aff1f323a2fac40158157cd25c41681f2
aoki-hid <aoki-hid@spp.co.jp>
2017-10-09 17:19:01 +0900

    Add Q4

--- ProgramingQ/Q3/Q3/Program.cs
+++ ProgramingQ/Q3/Q3/Program.cs
@@ -12,16 +12,18 @@ namespace Q3
         {
             var startDate = DateTime.Parse("2000/01/01");
             var endDate = DateTime.Parse("2014/01/01");
+            var day = DayOfWeek.Friday;
+            var date = 13;
 
-            Console.WriteLine(GetFridayThe13th(startDate, endDate).Select(x => x.ToShortDateString()).Aggregate((s, n) => s + Environment.NewLine + n));
+            Console.WriteLine(GetDayOfDate(startDate, endDate, day, date).Select(x => x.ToShortDateString()).Aggregate((s, n) => s + Environment.NewLine + n));
 
             Console.WriteLine("終了するには何かキーを押してください...");
             Console.ReadKey();
         }
 
-        static IEnumerable<DateTime> GetFridayThe13th(DateTime start, DateTime end)
+        static IEnumerable<DateTime> GetDayOfDate(DateTime start, DateTime end, DayOfWeek day, int date)
         {
-            return Enumerable.Range(0, int.MaxValue).Select(x => start.AddDays(x)).TakeWhile(d => d < end).Where(d => d.Day == 13).Where(d => d.DayOfWeek == DayOfWeek.Friday);
+            return Enumerable.Range(0, int.MaxValue).Select(x => start.AddDays(x)).TakeWhile(d => d < end).Where(d => d.Day == date).Where(d => d.DayOfWeek == day);
         }
     }
 }
---
+++ ProgramingQ/Q4/Q4/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value1 = "send";
+            var value2 = "more";
+            var answer = "money";
+
+            SolveAlphametic(value1, value2, answer);
+            //for (var s = 0; s < 10; s++)
+            //{
+            //    for (var e = s; e < 10; e++)
+            //    {
+            //        for (var n = e; n < 10; n++)
+            //        {
+            //            for (var d = n; d < 10; d++)
+            //            {
+            //                for (var m = d; m < 10; m++)
+            //                {
+            //                    for (var o = 0; o < 10; o++)
+            //                    {
+            //                        for (var r = 0; r < 10; r++)
+            //                        {
+            //                            for (var y = 0; y < 10; y++)
+            //                            {
+            //                                var send = s * 1000 + e * 100 + n * 10 + d;
+            //                                var more = m * 1000 + o * 100 + r * 10 + e;
+            //                                var money = m * 10000 + o * 1000 + n * 100 + e * 10 + y;
+            //                                if (s == 0) { continue; }
+            //                                if (m == 0) { continue; }
+            //                                if (s == e || s == n || s == d || s == m || s == o || s == r || s == y) { continue; }
+            //                                if (e == n || e == d || e == m || e == o || e == r || e == y) { continue; }
+            //                                if (n == d || n == m || n == o || n == r || n == y) { continue; }
+            //                                if (d == m || d == o || d == r || d == y) { continue; }
+            //                                if (m == o || m == r || m == y) { continue; }
+            //                                if (o == r || o == y) { continue; }
+            //                                if (r == y) { continue; }
+            //                                if (send + more == money)
+            //                                {
+            //                                    Console.WriteLine($"{s},{e},{n},{d},{m},{o},{n},{e},{y}");
+            //                                }
+            //                            }
+            //                        }
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.ReadKey();
+        }
+
+        static void SolveAlphametic(string value1, string value2, string answer)
+        {
+            var allString = new List<string>() { value1, value2, answer };
+            var firstChar = allString.Select(x=>x.First());
+            var allChar = firstChar.Concat(allString.Aggregate((s, n) => s + n).ToString().Except(firstChar)).Distinct();
+
+            Console.WriteLine(allChar.Aggregate("", (a, b) => a + b));
+            Console.WriteLine(firstChar.Aggregate("", (a, b) => a + b));
+
+
+        }
+    }
+}
