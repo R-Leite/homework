63f2dbd80a767c04347d4dbbd08478b4edf29fae
aoki-hid <aoki-hid@spp.co.jp>
2017-10-16 18:07:52 +0900

    Modify Q4

--- ProgramingQ/Q4/Q4/Program.cs
+++ ProgramingQ/Q4/Q4/Program.cs
@@ -14,14 +14,8 @@ namespace Q4
             var value2 = "more";
             var answer = "money";
 
-            var list = new List<int>() { 0, 1, 2, 3 };
+            SolveAlphametic(value1, value2, answer);
 
-            var hoge = Permutation(3, list).ToList();
-            Console.WriteLine(hoge);
-            foreach(var i in hoge)
-            {
-                Console.WriteLine(i.Select(x => x.ToString()).Aggregate((a, b)=>a + "," + b));
-            }
             Console.WriteLine("終了するには何かキーを押してください。");
             Console.ReadKey();
         }
@@ -29,19 +23,37 @@ namespace Q4
         static void SolveAlphametic(string value1, string value2, string answer)
         {
             var allString = new List<string>() { value1, value2, answer };
-            var firstChar = allString.Select(x=>x.First());
-            var allChar = firstChar.Concat(allString.Aggregate((s, n) => s + n).ToString().Except(firstChar)).Distinct();
+            var firstChars = allString.Select(x=>x.First()).Distinct();
+            var allChars = firstChars.Concat(allString.Aggregate((s, n) => s + n).ToString().Except(firstChars)).Distinct();
+            var firstCharsNumber = firstChars.Count();
 
-            Console.WriteLine(allChar.Aggregate("", (a, b) => a + b));
-            Console.WriteLine(firstChar.Aggregate("", (a, b) => a + b));
+            var aaa = Enumerable.Range(0, 10).Permutation(allChars.Count()).Where(x => x.Select((y, i) => (i < firstCharsNumber & y == 0)).Aggregate((a, b) => a || b))
+                .Select(x => allChars.Zip(x, (k, v) => new { k, v }).ToDictionary(a => a.k, a => a.v))
+                .Where(x => answer.Select(y => x[y]).Aggregate((a, b) => a * 10 + b) == value1.Select(y => x[y]).Aggregate((a, b) => a * 10 + b) + value2.Select(y => x[y]).Aggregate((a, b) => a * 10 + b));
+
+            foreach(var i in aaa) { Console.WriteLine(i); }
+            //foreach (var i in Enumerable.Range(0, 10).Permutation(allChars.Count()))
+            //{
+            //    if (Math.Abs(i.ToList().IndexOf(0)) < firstCharsNumber) { continue; }
+            //    var hoge = allChars.Zip(i, (k, v) => new { k, v }).ToDictionary(a => a.k, a => a.v);
+            //    if (answer.Select(x => hoge[x]).Aggregate((a, b) => a * 10 + b) == value1.Select(x => hoge[x]).Aggregate((a, b) => a * 10 + b) + value2.Select(x => hoge[x]).Aggregate((a, b) => a * 10 + b))
+            //    {
+            //        foreach (var j in hoge)
+            //        {
+            //            Console.WriteLine(j);
+            //        }
+            //    }
+            //}
 
 
         }
-
-        static IEnumerable<IEnumerable<T>> Permutation<T>(int n, IEnumerable<T> list)
+    }
+    static class IEnumerableExtension
+    {
+        public static IEnumerable<IEnumerable<T>> Permutation<T>(this IEnumerable<T> list, int n)
         {
             if (n <= 0) return new List<List<T>>() { Enumerable.Empty<T>().ToList() };
-            return list.SelectMany(head => Permutation(n - 1, list.Where(i => !head.Equals(i))).Select(tail => tail.Concat(Enumerable.Repeat(head, 1))));
+            return list.SelectMany(head => list.Where(i => !head.Equals(i)).Permutation(n - 1).Select(tail => tail.Concat(Enumerable.Repeat(head, 1))));
         }
     }
 }
