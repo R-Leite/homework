efa5424f9ddb1de7d8bde093113074ebde322a3f
aoki-hid <aoki-hid@spp.co.jp>
2017-11-07 19:00:24 +0900

    Add Sample

---
+++ ProgramingQ/calc/calc/BaseViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Sample
+{
+    class BaseViewModel : INotifyPropertyChanged, IDataErrorInfo
+    {
+        #region インスタンス変数
+        /// <summary> 
+        /// プロパティに紐づいたエラーメッセージ格納用の辞書 
+        /// </summary> 
+        protected Dictionary<string, string> _errors = new Dictionary<string, string>();
+
+        // 設定を保存するXMLファイル名
+        protected string _xmlFileName = @"GearCombination_SaveData.xml";
+        #endregion
+
+        #region プロパティ
+        /// <summary>
+        /// IDataErrorInfo.Errorの実装
+        /// </summary>
+        public string Error { get { return null; } }
+
+        /// <summary> 
+        /// columnNameで指定したプロパティのエラー 
+        /// </summary> 
+        /// <param name="columnName">プロパティ名</param> 
+        /// <returns>エラーメッセージ</returns> 
+        public string this[string columnName]
+        {
+            get { return (this._errors.ContainsKey(columnName) ? this._errors[columnName] : null); }
+        }
+        #endregion
+
+        #region イベントハンドラ
+        /// <summary> 
+        /// プロパティの変更時イベント
+        /// </summary> 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary> 
+        /// バインドされたプロパティのイベントハンドラ
+        /// </summary> 
+        /// <param name="propertyName">プロパティ名</param> 
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+        #endregion
+
+        #region エラーハンドリング
+        /// <summary> 
+        /// プロパティにエラーメッセージを設定する。 
+        /// </summary> 
+        /// <param name="propertyName">プロパティ名</param> 
+        /// <param name="errorMessage">エラーメッセージ</param> 
+        protected void SetError(string propertyName, string errorMessage)
+        {
+            this._errors[propertyName] = errorMessage;
+            this.RaisePropertyChanged(propertyName);
+        }
+
+        /// <summary> 
+        /// プロパティのエラーをクリアする。 
+        /// </summary> 
+        /// <param name="propertyName">プロパティ名</param> 
+        protected void ClearError(string propertyName)
+        {
+            if (this._errors.ContainsKey(propertyName))
+            {
+                this._errors.Remove(propertyName);
+            }
+            this.RaisePropertyChanged(propertyName);
+        }
+        #endregion
+    }
+}
---
+++ ProgramingQ/calc/calc/ViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    class ViewModel : BaseViewModel
+    {
+        private string _text;
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+        private IEnumerable<string> _items;
+        public IEnumerable<string> Items
+        {
+            get { return _items; }
+            set { _items = value; this.RaisePropertyChanged("Items"); }
+        }
+
+        private bool _check;
+        public bool Check
+        {
+            get { return _check; }
+            set
+            {
+                _check = value;
+                if (_check) _items = new List<string>() { "hoge", "fuga", "foo", "bar" };
+                else _items = new List<string>();
+                this.RaisePropertyChanged("Check");
+            }
+        }
+
+        public ViewModel()
+        {
+            _text = "Init";
+            _check = true;
+            //_items = new List<string>() { "hoge", "fuga", "foo", "bar" };
+            _items = new List<string>();
+        }
+    }
+}
