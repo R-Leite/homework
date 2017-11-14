389cb30724c00ece449f5bc8de295a94d41afbfa
aoki-hid <aoki-hid@spp.co.jp>
2017-11-14 19:10:57 +0900

    Modify Q7

--- ProgramingQ/Q7/Q7/Program.cs
+++ ProgramingQ/Q7/Q7/Program.cs
@@ -10,22 +10,35 @@ namespace Q7
     {
         static void Main(string[] args)
         {
-            Queue<string> queue = new Queue<string>();
+            Console.Write("現在温度：");
+            var currentTemp = int.Parse(Console.ReadLine());
+            Console.Write("設定温度：");
+            var settingTemp = int.Parse(Console.ReadLine());
+
+            var max = 35;
+            var min = 15;
+            var operate = new List<int>() { -10, 10, -5, 5, -1, 1 };
+            var already = new List<int>();
+            var queue = new Queue<int>();
+            var count = 0;
+
+            queue.Enqueue(currentTemp);
 
             while (queue.Count != 0)
             {
                 var currentNode = queue.Dequeue();
-                if(currentNode.IsGoal)
-                {
-                    break;
-                }
-                foreach(var pos in currentNode.NextPositions())
-                {
-                    var next = currentNode.Clone().ToString();
-                    next.Visited = true;
-                    queue.Enqueue(next);
-                }
+                Console.WriteLine(currentNode);
+                var operateTemp = operate.Select(x => x + currentNode).Where(x => x <= max).Where(x => x >= min).Where(x => !already.Contains(x)).ToList();
+                count++;
+                if (operateTemp.Contains(settingTemp)) break;
+                already.AddRange(operateTemp);
+
+                operateTemp.ForEach(x => queue.Enqueue(x));
             }
+
+            Console.WriteLine(count);
+            Console.WriteLine("終了するには何かキーを押してください...");
+            Console.ReadKey();
         }
     }
 }
