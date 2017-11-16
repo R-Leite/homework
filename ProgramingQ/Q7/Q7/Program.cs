454ae80a0918d3d300ba234e4fde1bbb81a4b99d
Leite <chemistry_nothing@hotmail.co.jp>
2017-11-17 07:11:54 +0900

    modify

--- ProgramingQ/Q7/Q7/Program.cs
+++ ProgramingQ/Q7/Q7/Program.cs
@@ -19,19 +19,22 @@ namespace Q7
             var min = 15;
             var operate = new List<int>() { -10, 10, -5, 5, -1, 1 };
             var already = new List<int>();
-            var queue = new Queue<int>();
+            var queue = new Queue<Tuple<string, int>>();
             var count = 0;
 
-            queue.Enqueue(currentTemp);
+            queue.Enqueue(Tuple.Create<string, int>("", currentTemp));
 
             while (queue.Count != 0)
             {
                 var currentNode = queue.Dequeue();
-                Console.WriteLine(currentNode);
-                var operateTemp = operate.Select(x => x + currentNode).Where(x => x <= max).Where(x => x >= min).Where(x => !already.Contains(x)).ToList();
-                count++;
-                if (operateTemp.Contains(settingTemp)) break;
-                already.AddRange(operateTemp);
+//                Console.WriteLine(currentNode.Item1);
+                var operateTemp = operate.Select(x => Tuple.Create<string, int>(currentNode.Item1 + x.ToString(), x + currentNode.Item2)).Where(x => x.Item2 <= max).Where(x => x.Item2 >= min).Where(x => !already.Contains(x.Item2)).ToList();
+                if (operateTemp.Select(x => x.Item2).Contains(settingTemp))
+                {
+                    Console.WriteLine(currentNode.Item1);
+                    break;
+                }
+                already.AddRange(operateTemp.Select(x => x.Item2));
 
                 operateTemp.ForEach(x => queue.Enqueue(x));
             }
