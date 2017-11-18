2390992d1d6715948ca4fa1c0795a8a7fe5821d7
Leite <chemistry_nothing@hotmail.co.jp>
2017-11-19 01:39:16 +0900

    Modify Q7

--- ProgramingQ/Q7/Q7/AirConditioner.cs
+++ ProgramingQ/Q7/Q7/AirConditioner.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Q7
 {
     class AirConditioner
     {
+        private IEnumerable<int> _process;
+        private int _currentTemperature;
+
+        private const int MAX = 35;
+        private const int MIN = 15;
+
+        public static int SettingTemperature;
+
+        static IEnumerable<int> operateList;
+        static IEnumerable<int> already;
+
+        public AirConditioner(IEnumerable<int> process, int temp)
+        {
+            _process = process;
+            _currentTemperature = temp;
+        }
+
+        static AirConditioner()
+        {
+            operateList = new List<int>() { -10, 10, -5, 5, -1, 1 };
+            already = new List<int>();
+        }
+
+        public IEnumerable<AirConditioner> GetChildren()
+        {
+            return operateList.Select(x => new AirConditioner(_process.Concat(Enumerable.Repeat(x, 1)), x + _currentTemperature)).Where(x => x._currentTemperature >= MIN).Where(x => x._currentTemperature <= MAX).Where(x => !already.Contains(x._currentTemperature));
+        }
+
+        public bool isGoal()
+        {
+            return _currentTemperature == SettingTemperature;
+        }
+
+        public void ShowResult()
+        {
+            Console.WriteLine($"最小操作手順：{_process.Count()}");
+            Console.WriteLine($"操作手順：{_process.Select(x => x.ToString()).Aggregate((a, b) => a + " -> " + b)}");
+        }
     }
 }
--- ProgramingQ/Q7/Q7/Program.cs
+++ ProgramingQ/Q7/Q7/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Q7
 {
@@ -12,35 +10,24 @@ namespace Q7
         {
             Console.Write("現在温度：");
             var currentTemp = int.Parse(Console.ReadLine());
+
             Console.Write("設定温度：");
-            var settingTemp = int.Parse(Console.ReadLine());
+            AirConditioner.SettingTemperature =  int.Parse(Console.ReadLine());
 
-            var max = 35;
-            var min = 15;
-            var operate = new List<int>() { -10, 10, -5, 5, -1, 1 };
-            var already = new List<int>();
-            var queue = new Queue<Tuple<string, int, int>>();
+            var queue = new Queue<AirConditioner>();
 
-            queue.Enqueue(Tuple.Create<string,int, int>("", 0, currentTemp));
+            queue.Enqueue(new AirConditioner(new List<int>(), currentTemp));
 
             while (queue.Count != 0)
             {
                 var currentNode = queue.Dequeue();
-                if(currentNode.Item3 == settingTemp)
+                if(currentNode.isGoal())
                 {
-                    Console.WriteLine(currentNode.Item1);
-                    Console.WriteLine(currentNode.Item2);
+                    currentNode.ShowResult();
                     break;
                 }
-                var operateTemp = operate.Select(x => Tuple.Create<string, int, int>(currentNode.Item1 + " -> " + x.ToString(), currentNode.Item2 + 1, x + currentNode.Item3)).Where(x => x.Item2 <= max).Where(x => x.Item3 >= min).Where(x => !already.Contains(x.Item3)).ToList();
-                //if (operateTemp.Select(x => x.Item2).Contains(settingTemp))
-                //{
-                //    Console.WriteLine(currentNode.Item1);
-                //    break;
-                //}
-                already.AddRange(operateTemp.Select(x => x.Item3));
 
-                operateTemp.ForEach(x => queue.Enqueue(x));
+                currentNode.GetChildren().ToList().ForEach(x => queue.Enqueue(x));
             }
 
             Console.WriteLine("終了するには何かキーを押してください...");
