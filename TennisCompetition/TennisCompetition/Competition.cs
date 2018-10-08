e5250a95330b0caf266440ea79404f31d3002f3f
aoki-hid <aoki-hid@spp.co.jp>
2018-10-08 17:16:44 +0900

    Tennis

---
+++ TennisCompetition/TennisCompetition/Competition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisCompetition
+{
+    class Competition
+    {
+        // 出場回数、各プレイヤーとのコンビ回数、同コード回数の格納変数を用意する
+        public int part;
+        public List<int> player_combi;
+        public List<int> player_coat;
+
+        public Competition()
+        {
+            part = 0;
+            player_combi = new List<int> { 0, 0, 0, 0, 0, 0, 0, 0 };
+            player_coat = new List<int> { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            // 一番少ない順に出力していく
+        }
+    }
+}
---
+++ TennisCompetition/TennisCompetition/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisCompetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 40;
+            Console.WindowWidth = 150;
+
+            try
+            {
+                Console.WriteLine("テニスの対戦組み合わせ表を出力します");
+                var p1 = new Competition();
+                while(true)
+                {
+                    var list = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+                }
+            }
+            finally
+            {
+                Console.WriteLine("続行するには何かキーを押してください。");
+                Console.ReadKey();
+            }
+        }
+    }
+}
