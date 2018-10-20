9d1915dcdbaf0b11ceca831334b031f5fee6280e
Leite <chemistry_nothing@hotmail.co.jp>
2018-10-21 00:56:00 +0900

    Modify tennis

---
+++ TennisCompetition/TennisCompetition/Pair.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisCompetition
+{
+    class Pair
+    {
+        public int _player1;
+        public int _player2;
+
+        public Pair(int p1, int p2)
+        {
+            _player1 = p1;
+            _player2 = p2;
+        }
+
+        public bool contains(Pair p)
+        {
+            if (_player1 == p._player1) { return true; }
+            if (_player1 == p._player2) { return true; }
+            if (_player2 == p._player1) { return true; }
+            if (_player2 == p._player2) { return true; }
+            return false;
+        }
+    }
+}
--- TennisCompetition/TennisCompetition/Program.cs
+++ TennisCompetition/TennisCompetition/Program.cs
@@ -16,16 +16,34 @@ namespace TennisCompetition
             try
             {
                 Console.WriteLine("テニスの対戦組み合わせ表を出力します");
-                var p1 = new Competition();
+                var pairs = new List<Pair>();
 
                 // 全ペアを作成する
-                for(var i = 0; i < 8; i++)
+                for(var i = 1; i <= 8; i++)
                 {
-                    for (var j = i+1; j < 8; j++)
+                    for (var j = i + 1; j <= 8; j++)
                     {
-                        Console.WriteLine(i.ToString()+","+j.ToString());
+                        pairs.Add(new Pair(i, j));
                     }
                 }
+
+                //pairs.ForEach(x => Console.WriteLine(x._player1.ToString() +","+x._player2.ToString()));
+
+                // ペアを4つ組み合わせて、全ペアがなくなるまで繰り返す
+                var cpairs = pairs.ToList();
+                var answers = new List<Pair>();
+                answers.Add(new Pair(1, 2));
+                while (cpairs.Count() > 0)
+                {
+                    var hoge = cpairs.Where(x => !answers.Any(y => y.contains(x))).First();
+                    Console.WriteLine(hoge._player1.ToString() + "," + hoge._player2.ToString());
+                    //                    answers.Add(cpairs.Where(x => answers.Any(y => !y.contains(x))).First());
+                    answers.Add(hoge);
+                    cpairs.Remove(hoge);
+                }
+                Console.WriteLine();
+                answers.ForEach(x => Console.WriteLine(x._player1.ToString() + "," + x._player2.ToString()));
+
             }
             finally
             {
