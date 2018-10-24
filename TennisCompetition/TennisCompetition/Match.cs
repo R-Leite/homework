a16bc019f09bba0e538337f72bebbd1391fa1fe0
Leite <chemistry_nothing@hotmail.co.jp>
2018-10-24 23:41:07 +0900

    Modify tennis

---
+++ TennisCompetition/TennisCompetition/Match.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisCompetition
+{
+    class Match
+    {
+        public Pair Pair1;
+        public Pair Pair2;
+        public Pair Pair3;
+        public Pair Pair4;
+
+        public Match(Pair p1, Pair p2, Pair p3, Pair p4)
+        {
+            Pair1 = p1;
+            Pair2 = p2;
+            Pair3 = p3;
+            Pair4 = p4;
+        }
+
+        public bool contains(Match m)
+        {
+            if (Pair1 == m.Pair1) { return true; }
+            if (Pair1 == m.Pair2) { return true; }
+            if (Pair1 == m.Pair3) { return true; }
+            if (Pair1 == m.Pair4) { return true; }
+
+            if (Pair2 == m.Pair1) { return true; }
+            if (Pair2 == m.Pair2) { return true; }
+            if (Pair2 == m.Pair3) { return true; }
+            if (Pair2 == m.Pair4) { return true; }
+
+            if (Pair3 == m.Pair1) { return true; }
+            if (Pair3 == m.Pair2) { return true; }
+            if (Pair3 == m.Pair3) { return true; }
+            if (Pair3 == m.Pair4) { return true; }
+
+            if (Pair4 == m.Pair1) { return true; }
+            if (Pair4 == m.Pair2) { return true; }
+            if (Pair4 == m.Pair3) { return true; }
+            if (Pair4 == m.Pair4) { return true; }
+
+            return false;
+        }
+    }
+}
--- TennisCompetition/TennisCompetition/Program.cs
+++ TennisCompetition/TennisCompetition/Program.cs
@@ -15,34 +15,75 @@ namespace TennisCompetition
 
             try
             {
-                Console.WriteLine("テニスの対戦組み合わせ表を出力します");
-                var input = Console.ReadLine();
+                Console.WriteLine("テニスの対戦組み合わせ表を出力します。");
+                Console.Write("人数を入力して下さい。：");
+                var playerNumber = int.Parse(Console.ReadLine());
+
                 // プレイヤークラスも必要だとおもう
 
                 // ペアクラス
                 var pairs = new List<Pair>();
 
                 // 全ペアを作成する
-                for(var i = 1; i <= 8; i++)
+                for(var i = 1; i <= playerNumber; i++)
                 {
-                    for (var j = i + 1; j <= 8; j++)
+                    for (var j = i + 1; j <= playerNumber; j++)
                     {
                         pairs.Add(new Pair(i, j));
                     }
                 }
 
+                var matches = new List<Match>();
                 for (var i = 0; i < pairs.Count; i++)
                 {
                     for (var j = i + 1; j < pairs.Count; j++)
                     {
-                        if (!pairs[i].contains(pairs[j]))
+                        for (var k = j + 1; k < pairs.Count; k++)
                         {
-                            Console.WriteLine(pairs[i]._player1.ToString() + ", " + pairs[i]._player2.ToString());
-                            Console.WriteLine(pairs[j]._player1.ToString() + ", " + pairs[j]._player2.ToString());
-                            Console.WriteLine();
+                            for (var l = k + 1; l < pairs.Count; l++)
+                            {
+                                if (!pairs[i].contains(pairs[j]) && !pairs[i].contains(pairs[k]) && !pairs[i].contains(pairs[l]))
+                                {
+                                    if (!pairs[j].contains(pairs[k]) && !pairs[j].contains(pairs[l]))
+                                    {
+                                        if (!pairs[k].contains(pairs[l]))
+                                        {
+                                            matches.Add(new Match(pairs[i], pairs[j], pairs[k], pairs[l]));
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                 }
+
+                var copyMatches = matches.ToList();
+                var ansMatches = new List<Match>();
+
+                while (true)
+                {
+                    if (ansMatches.Count == 0)
+                    {
+                        var m = copyMatches.First();
+                        ansMatches.Add(m);
+                        copyMatches.Remove(m);
+                        continue;
+                    }
+
+                    var mm = copyMatches.Where(x => !ansMatches.Any(y=>y.contains(x))).FirstOrDefault();
+                    if (mm == null)
+                    {
+                        break;
+                    }
+                    Console.WriteLine(mm.Pair1._player1.ToString() + "," + mm.Pair1._player2.ToString() + "," + mm.Pair2._player1.ToString() + "," + mm.Pair2._player2.ToString() + "," + mm.Pair3._player1.ToString() + "," + mm.Pair3._player2.ToString() + "," + mm.Pair4._player1.ToString() + "," + mm.Pair4._player2.ToString());
+                    
+                    ansMatches.Add(mm);
+                    copyMatches.Remove(mm);
+                }
+
+                Console.WriteLine();
+                ansMatches.ForEach(x => Console.WriteLine(x.Pair1._player1.ToString()+","+x.Pair1._player2.ToString()+","+ x.Pair2._player1.ToString() + "," + x.Pair2._player2.ToString() + "," + x.Pair3._player1.ToString() + "," + x.Pair3._player2.ToString() + "," + x.Pair4._player1.ToString() + "," + x.Pair4._player2.ToString()));
+
                 //pairs.ForEach(x => Console.WriteLine(x._player1.ToString() +","+x._player2.ToString()));
 
                 // ペアを4つ組み合わせて、全ペアがなくなるまで繰り返す
@@ -63,7 +104,7 @@ namespace TennisCompetition
                     var skip = answers.Count / 4 * 4;
 //                    Console.WriteLine(skip);
                     var hoge = cpairs.Where(x => !answers.Skip(answers.Count / 4 * 4).Any(y => y.contains(x))).First();
-                    Console.WriteLine(hoge._player1.ToString() + "," + hoge._player2.ToString());
+                    //Console.WriteLine(hoge._player1.ToString() + "," + hoge._player2.ToString());
                     //                    answers.Add(cpairs.Where(x => answers.Any(y => !y.contains(x))).First());
                     answers.Add(hoge);
                     cpairs.Remove(hoge);
