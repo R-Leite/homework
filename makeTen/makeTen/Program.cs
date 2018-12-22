619794a8f3c0673e5289e8437f1b4eb7b0497080
Leite <chemistry_nothing@hotmail.co.jp>
2018-12-23 01:44:59 +0900

    Add makeTen

---
+++ makeTen/makeTen/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace makeTen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberList = Enumerable.Range(1, 9);
+            var numberCombinationList = numberList.SelectMany(a =>
+            numberList.SelectMany(b =>
+            numberList.SelectMany(c =>
+            numberList.Select(d => new NumberCombination(a, b, c, d)))));
+
+            foreach(var i in numberCombinationList)
+            {
+                // 同じ数字があるやつはスキップ
+//                if (i.ContainsSameNumber()) { continue; }
+
+                if (i.isMakeTen())
+                {
+//                    Console.WriteLine(i.ToString());
+                }
+                else
+                {
+                    Console.WriteLine(i.ToString());
+                }
+            }
+
+            Console.WriteLine("何かキーを押してください。");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ makeTen/makeTen/numberCombination.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace makeTen
+{
+    class NumberCombination
+    {
+        private readonly int num1;
+        private readonly int num2;
+        private readonly int num3;
+        private readonly int num4;
+        private Dictionary<string, Func<double, double, double>> _calcDict = new Dictionary<string, Func<double, double, double>>
+        {
+            {"+", (x, y) => x + y},
+            {"-", (x, y) => x - y},
+            {"*", (x, y) => x * y},
+            {"/", (x, y) => x / y}
+        };
+
+        public NumberCombination(int n1, int n2, int n3, int n4)
+        {
+            this.num1 = n1;
+            this.num2 = n2;
+            this.num3 = n3;
+            this.num4 = n4;
+        }
+
+        public override string ToString()
+        {
+            return "(" + this.num1 + ", " + this.num2 + ", " + this.num3 + ", " + this.num4 + ")";
+        }
+
+        // 同じ数字がある？
+        public bool ContainsSameNumber()
+        {
+            if (this.num1 == this.num2) { return true; }
+            if (this.num1 == this.num3) { return true; }
+            if (this.num1 == this.num4) { return true; }
+            if (this.num2 == this.num3) { return true; }
+            if (this.num2 == this.num4) { return true; }
+            if (this.num3 == this.num4) { return true; }
+
+            return false;
+        }
+
+        public bool isMakeTen()
+        {
+            var operatorList = new List<string>() { "+", "-", "*", "/" };
+            var answerOperators = operatorList.SelectMany(_ => operatorList, (x, y) => new { x, y })
+            .SelectMany(_ => operatorList, (_, z) => new { _.x, _.y, z })
+            .Where(_ => this.Calculate(_.z, this.Calculate(_.y, this.Calculate(_.x, this.num1, this.num2), this.num3), this.num4) == 10.0);
+
+            foreach (var ans in answerOperators)
+            {
+                Console.WriteLine(this.num1 + ans.x + this.num2 + ans.y + this.num3 + ans.z + this.num4);
+            }
+
+            return true;
+        }
+
+        // 四則演算の計算結果を返す
+        private double Calculate(string operater, double a, double b)
+        {
+            return _calcDict[operater](a, b);
+        }
+    }
+}
